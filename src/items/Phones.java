993e9e7da6fc9826cf4d3f14da07b1258638b964
Chander7 <Chander7@users.noreply.github.com>
2016-09-20 15:30:55 +0200

    Update Phones.java

--- src/items/Phones.java
+++ src/items/Phones.java
@@ -1,9 +1,35 @@
 public class Phones {
 
-private string phoneName;
-private string
+private String phoneName;
+private String phoneModel;
+private String phoneCompany;
 
+private String phoneImei;
+private String phoneColor;
 
+private Long phoneNumber;
+
+Boolean network3g;
+Boolean network4g;
+
+//Phonecompany
+
+public String getPhoneCompany () {
+  return this.phoneCompany;
+}
+
+public void setPhoneCompany (String pc) {
+  String [] a = {"Samsung", "Apple", "Microsoft", "Huawei"};
+}
+
+//phoneName
+public String getPhoneName () {
+  return this.phoneName;
+}
+
+public void setPhoneName (String pn) {
+  this.phoneName = pn;
+}
 
 
 }
