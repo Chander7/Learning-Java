976c200623abc3568200c5eb7a3dd24553fa79b8
Chander7 <Chander7@users.noreply.github.com>
2016-09-22 09:09:03 +0200

    Update StartHere.java

--- src/StartHere.java
+++ src/StartHere.java
@@ -1,6 +1,10 @@
-package items
+package items;
+
 public class StartHere {
   Public static void main (String [] args ) {
+    Phones psamsung = new Phones ();
+    psamsung.setPhoneCompany ("Samsung")'
+    psamsung.getPhoneCompany ();
     
   
  }
